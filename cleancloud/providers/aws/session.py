31f6db3c5c2c807b8573f6250badaf126125c599
sureshcsdp <241378668+sureshcsdp@users.noreply.github.com>
2026-02-23 23:21:39 +0000

    Fix AWS cloudshell Python version issue

--- cleancloud/providers/aws/session.py
+++ cleancloud/providers/aws/session.py
@@ -1,8 +1,10 @@
+from typing import Optional
+
 import boto3
 from boto3.session import Session
 
 
-def create_aws_session(profile: str | None, region: str) -> Session:
+def create_aws_session(profile: Optional[str], region: str) -> Session:
     if profile:
         return boto3.Session(profile_name=profile, region_name=region)
 
