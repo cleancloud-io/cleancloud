da13aa587af4df86baaa0e1683e943d847c444c7
sureshcsdp <suresh.csdp@gmail.com>
2026-04-11 20:04:41 +0100

    Minor feedback review changes (#140)

--- cleancloud/output/feedback.py
+++ cleancloud/output/feedback.py
@@ -25,5 +25,3 @@ def show_feedback_prompt():
     click.echo()
     click.echo("Report any issues: https://github.com/cleancloud-io/cleancloud/issues")
     click.echo()
-    click.echo("Or email: suresh@getcleancloud.com")
-    click.echo()
